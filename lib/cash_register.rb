25b0e67c8ddaaaac555918931c7554b33eef8837
Karen Daniela <kdaniimora@gmail.com>
2020-02-17 21:41:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,20 @@
-def CashRegister
- attr_accessor :total, :apply_discount, :items, :void_last_transaction
+
+  
+  class CashRegister 
+  attr_accessor :total, :discount, :items, :last_transaction
+  
   
   def initialize(discount = 0)
-    @total = 0 
-    @apply_discount = apply_discount
-    @item = []
-    end 
+    @total = 0
+    @discount = discount
+    @items = []
+  end 
   
-  def total
-    @total
-  end
-  
-    def add_item(title, price, quantity = 1) 
+  def total 
+    @total 
+  end 
+
+  def add_item(title, price, quantity = 1) 
     quantity == 1 ? self.total+= price : self.total += price * quantity
     @items.fill(title, @items.length , quantity)
     @last_transaction = price * quantity
@@ -25,15 +28,13 @@ def CashRegister
       self.total = total.to_i - total_disc
       return "After the discount, the total comes to $#{self.total}."
     end
-  end
+  end 
   
   def items 
-    @items = []
-end 
+    @items
+  end 
   
   def void_last_transaction
     @total = @total - @last_transaction
+  end 
 end 
-  
-end 
-  
